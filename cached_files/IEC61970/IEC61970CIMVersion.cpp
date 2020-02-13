7300e6532555e2e4df5134ae6e50d1437d1046d9
Richard Marston <rmarston@eonerc.rwth-aachen.de>
2020-02-13 09:59:40 +0100

    Remove namespace in cim version

--- cached_files/IEC61970/IEC61970CIMVersion.cpp
+++ cached_files/IEC61970/IEC61970CIMVersion.cpp
@@ -11,5 +11,5 @@ IEC61970CIMVersion::IEC61970CIMVersion(){}
 
 IEC61970CIMVersion::~IEC61970CIMVersion(){}
 
-const Date IEC61970CIMVersion::date = IEC61970::Base::Domain::Date("2017-07-26");
+const Date IEC61970CIMVersion::date = Date("2017-07-26");
 const String IEC61970CIMVersion::version = "IEC61970CIM17v23";
