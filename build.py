1966b5ae275009697108468dd81c623e4d214b15
iripiri <ikoester@eonerc.rwth-aachen.de>
2023-06-27 12:29:42 +0000

    make cgmes version a command line argument; version not hardcoded
    Signed-off-by: iripiri <ikoester@eonerc.rwth-aachen.de>

--- build.py
+++ build.py
@@ -7,11 +7,13 @@ parser = argparse.ArgumentParser(description='Generate some CIM classes.')
 parser.add_argument('outdir', type=str, help='The output directory')
 parser.add_argument('schemadir', type=str, help='The schema directory')
 parser.add_argument('langdir', type=str, help='The langpack directory')
+parser.add_argument('--cgmes_version', type=str, choices=['cgmes_v2_4_13', 'cgmes_v2_4_15', 'cgmes_v3_0_0'], default='cgmes_v2_4_15', help='CGMES Version')
 args = parser.parse_args()
 
 langPack = importlib.import_module(args.langdir + ".langPack")
-
 schema_path = os.path.join(os.getcwd(), args.schemadir)
-CIMgen.cim_generate(schema_path, args.outdir, "cgmes_v2_4_15", langPack)
+cgmes_version = args.cgmes_version
+
+CIMgen.cim_generate(schema_path, args.outdir, cgmes_version, langPack)
 
 langPack.resolve_headers(args.outdir)
