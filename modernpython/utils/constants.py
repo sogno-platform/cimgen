1af5e6bce5b6bd1d5d63a0a9ac22d3d9f3f7bf7c
Guillaume Roger <guillaume.roger@alliander.com>
2023-10-20 09:40:53 +0200

    Tell Sonar that namespaces are safe
    Signed-off-by: Guillaume Roger <guillaume.roger@alliander.com>

--- modernpython/utils/constants.py
+++ modernpython/utils/constants.py
@@ -1,8 +1,8 @@
 # Default namespaces used by CGMES.
-NAMESPACES = {
-    "cim": "http://iec.ch/TC57/CIM100#",
-    "entsoe": "http://entsoe.eu/CIM/SchemaExtension/3/1#",
-    "md": "http://iec.ch/TC57/61970-552/ModelDescription/1#",
-    "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-    "xsd": "http://www.w3.org/2001/XMLSchema#",
+NAMESPACES = {  # Those are strings, not real addresses, hence the NOSONAR.
+    "cim": "http://iec.ch/TC57/CIM100#",  # NOSONAR
+    "entsoe": "http://entsoe.eu/CIM/SchemaExtension/3/1#",  # NOSONAR
+    "md": "http://iec.ch/TC57/61970-552/ModelDescription/1#",  # NOSONAR
+    "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",  # NOSONAR
+    "xsd": "http://www.w3.org/2001/XMLSchema#",  # NOSONAR
 }
