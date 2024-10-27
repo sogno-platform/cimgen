6e093343ca7f7d54d6d9ef193267318c6d2e88f0
Thomas Günther <tom@toms-cafe.de>
2024-10-27 20:47:07 +0100

    Add static cpp sources and parser classes (copied from libcimpp)
    Signed-off-by: Thomas Günther <tom@toms-cafe.de>

---
+++ cimgen/languages/cpp/src/CIMExceptions.cpp
@@ -0,0 +1,56 @@
+#include "CIMExceptions.hpp"
+
+
+CIMException::CIMException()
+{
+}
+
+CriticalError::CriticalError(const std::string& what) : runtime_error(what)
+{
+}
+
+MissingDependencyFile::MissingDependencyFile(const CIMModel* model, const std::string id)
+	: Model(model),
+	  rdfID(id)
+{
+	message = "CIMModel: Dependency is missing";
+}
+
+const char* CIMException::what() const noexcept
+{
+	return message.c_str();
+}
+
+
+
+MissingModelDescription::MissingModelDescription(const ModelDescriptionHandler* desc)
+	: DescriptionHandler(desc)
+{
+	message = "ModelDescriptionHandler: modelDescription not set";
+}
+
+
+NoObjectsContainer::NoObjectsContainer(const CIMContentHandler* handler)
+	: ContentHandler(handler)
+{
+	message = "CIMContentHandler: Object container not set";
+}
+
+
+NoRdfID::NoRdfID()
+{
+	message = "CIMContentHandler: Attributes contain no rdf:ID";
+}
+
+
+NoRdfMap::NoRdfMap(const CIMContentHandler* handler)
+	: ContentHandler(handler)
+{
+	message = "CIMContentHandler: RDFMap not set";
+}
+
+
+ReadingUninitializedField::ReadingUninitializedField()
+{
+	message = "Error: Uninitialized Field";
+}
