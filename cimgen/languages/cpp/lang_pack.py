450ff190600763273ef627c41f67b5e636f5a8a0
Thomas Günther <tom@toms-cafe.de>
2024-10-27 20:21:30 +0100

    Unify formatting of the generated cpp classes
    Signed-off-by: Thomas Günther <tom@toms-cafe.de>

--- cimgen/languages/cpp/lang_pack.py
+++ cimgen/languages/cpp/lang_pack.py
@@ -115,7 +115,7 @@ def insert_assign_fn(text, render):
     label = attribute_json["label"]
     class_name = attribute_json["domain"]
     return (
-        'assign_map.insert(std::make_pair(std::string("cim:'
+        '	assign_map.insert(std::make_pair(std::string("cim:'
         + class_name
         + "."
         + label
@@ -135,7 +135,7 @@ def insert_class_assign_fn(text, render):
     label = attribute_json["label"]
     class_name = attribute_json["domain"]
     return (
-        'assign_map.insert(std::make_pair(std::string("cim:'
+        '	assign_map.insert(std::make_pair(std::string("cim:'
         + class_name
         + "."
         + label
@@ -153,13 +153,13 @@ def create_nullptr_assigns(text, render):
         return ""
     else:
         attributes_json = eval(attributes_txt)
-        nullptr_init_string = ": "
+        nullptr_init_string = ""
         for attribute in attributes_json:
             if attribute["is_class_attribute"]:
                 nullptr_init_string += "LABEL(nullptr), ".replace("LABEL", attribute["label"])
 
     if len(nullptr_init_string) > 2:
-        return nullptr_init_string[:-2]
+        return " : " + nullptr_init_string[:-2]
     else:
         return ""
 
@@ -204,10 +204,13 @@ bool assign_OBJECT_CLASS_LABEL(BaseClass* BaseClass_ptr1, BaseClass* BaseClass_p
         else:
             assign = (
                 """
-bool assign_OBJECT_CLASS_LABEL(BaseClass* BaseClass_ptr1, BaseClass* BaseClass_ptr2) {
-	if(OBJECT_CLASS* element = dynamic_cast<OBJECT_CLASS*>(BaseClass_ptr1)) {
-		if(dynamic_cast<ATTRIBUTE_CLASS*>(BaseClass_ptr2) != nullptr) {
-                        element->LABEL.push_back(dynamic_cast<ATTRIBUTE_CLASS*>(BaseClass_ptr2));
+bool assign_OBJECT_CLASS_LABEL(BaseClass* BaseClass_ptr1, BaseClass* BaseClass_ptr2)
+{
+	if (OBJECT_CLASS* element = dynamic_cast<OBJECT_CLASS*>(BaseClass_ptr1))
+	{
+		if (dynamic_cast<ATTRIBUTE_CLASS*>(BaseClass_ptr2) != nullptr)
+		{
+			element->LABEL.push_back(dynamic_cast<ATTRIBUTE_CLASS*>(BaseClass_ptr2));
 			return true;
 		}
 	}
@@ -223,7 +226,8 @@ bool assign_OBJECT_CLASS_LABEL(BaseClass* BaseClass_ptr1, BaseClass* BaseClass_p
         assign = (
             """
 bool assign_INVERSEC_INVERSEL(BaseClass*, BaseClass*);
-bool assign_OBJECT_CLASS_LABEL(BaseClass* BaseClass_ptr1, BaseClass* BaseClass_ptr2) {
+bool assign_OBJECT_CLASS_LABEL(BaseClass* BaseClass_ptr1, BaseClass* BaseClass_ptr2)
+{
 	OBJECT_CLASS* element = dynamic_cast<OBJECT_CLASS*>(BaseClass_ptr1);
 	ATTRIBUTE_CLASS* element2 = dynamic_cast<ATTRIBUTE_CLASS*>(BaseClass_ptr2);
 	if (element != nullptr && element2 != nullptr)
@@ -247,13 +251,17 @@ bool assign_OBJECT_CLASS_LABEL(BaseClass* BaseClass_ptr1, BaseClass* BaseClass_p
     else:
         assign = (
             """
-bool assign_OBJECT_CLASS_LABEL(BaseClass* BaseClass_ptr1, BaseClass* BaseClass_ptr2) {
-	if(OBJECT_CLASS* element = dynamic_cast<OBJECT_CLASS*>(BaseClass_ptr1)) {
-                element->LABEL = dynamic_cast<ATTRIBUTE_CLASS*>(BaseClass_ptr2);
-                if(element->LABEL != nullptr)
-                        return true;
-        }
-        return false;
+bool assign_OBJECT_CLASS_LABEL(BaseClass* BaseClass_ptr1, BaseClass* BaseClass_ptr2)
+{
+	if(OBJECT_CLASS* element = dynamic_cast<OBJECT_CLASS*>(BaseClass_ptr1))
+	{
+		element->LABEL = dynamic_cast<ATTRIBUTE_CLASS*>(BaseClass_ptr2);
+		if (element->LABEL != nullptr)
+		{
+			return true;
+		}
+	}
+	return false;
 }""".replace(  # noqa: E101,W191
                 "OBJECT_CLASS", attribute_json["domain"]
             )
@@ -282,16 +290,17 @@ def create_assign(text, render):
     ):
         assign = (
             """
-bool assign_CLASS_LABEL(std::stringstream &buffer, BaseClass* BaseClass_ptr1) {
-	if(CLASS* element = dynamic_cast<CLASS*>(BaseClass_ptr1)) {
-                buffer >> element->LBL_WO_KEYWORD;
-                if(buffer.fail())
-                        return false;
-                else
-                        return true;
-        }
-        else
-                return false;
+bool assign_CLASS_LABEL(std::stringstream &buffer, BaseClass* BaseClass_ptr1)
+{
+	if (CLASS* element = dynamic_cast<CLASS*>(BaseClass_ptr1))
+	{
+		buffer >> element->LBL_WO_KEYWORD;
+		if (buffer.fail())
+			return false;
+		else
+			return true;
+	}
+	return false;
 }""".replace(  # noqa: E101,W191
                 "CLASS", attribute_json["domain"]
             )
@@ -300,10 +309,12 @@ bool assign_CLASS_LABEL(std::stringstream &buffer, BaseClass* BaseClass_ptr1) {
         )
     else:  # is_primitive_string_attribute
         assign = """
-bool assign_CLASS_LABEL(std::stringstream &buffer, BaseClass* BaseClass_ptr1) {
-	if(CLASS* element = dynamic_cast<CLASS*>(BaseClass_ptr1)) {
+bool assign_CLASS_LABEL(std::stringstream &buffer, BaseClass* BaseClass_ptr1)
+{
+	if (CLASS* element = dynamic_cast<CLASS*>(BaseClass_ptr1))
+	{
 		element->LABEL = buffer.str();
-		if(buffer.fail())
+		if (buffer.fail())
 			return false;
 		else
 			return true;
@@ -345,8 +356,8 @@ def _create_attribute_includes(text, render):
         for attribute in attributes:
             if attribute["is_primitive_attribute"] or attribute["is_enum_attribute"]:
                 unique[attribute["attribute_class"]] = True
-    for clarse in unique:
-        include_string += '\n#include "' + clarse + '.hpp"'
+    for clarse in sorted(unique):
+        include_string += '#include "' + clarse + '.hpp"\n'
     return include_string
 
 
@@ -361,8 +372,8 @@ def _create_attribute_class_declarations(text, render):
         for attribute in attributes:
             if attribute["is_class_attribute"] or attribute["is_list_attribute"]:
                 unique[attribute["attribute_class"]] = True
-    for clarse in unique:
-        include_string += "\nclass " + clarse + ";"
+    for clarse in sorted(unique):
+        include_string += "	class " + clarse + ";\n"
     return include_string
 
 
@@ -415,15 +426,15 @@ class_blacklist = [
 iec61970_blacklist = ["CIMClassList", "CIMNamespaces", "Folders", "Task", "IEC61970"]
 
 
-def _is_enum_class(filepath):
+def _is_primitive_or_enum_class(filepath):
     with open(filepath, encoding="utf-8") as f:
         try:
             for line in f:
-                if "enum class" in line:
-                    return True
+                if "static const BaseClassDefiner declare();" in line:
+                    return False
         except UnicodeDecodeError as error:
             print("Warning: UnicodeDecodeError parsing {0}: {1}".format(filepath, error))
-    return False
+    return True
 
 
 def _create_header_include_file(directory, header_include_filename, header, footer, before, after, blacklist):
@@ -434,7 +445,7 @@ def _create_header_include_file(directory, header_include_filename, header, foot
         filepath = os.path.join(directory, filename)
         basepath, ext = os.path.splitext(filepath)
         basename = os.path.basename(basepath)
-        if ext == ".hpp" and not _is_enum_class(filepath) and basename not in blacklist:
+        if ext == ".hpp" and not _is_primitive_or_enum_class(filepath) and basename not in blacklist:
             lines.append(before + basename + after)
     for line in lines:
         header.append(line)
@@ -452,13 +463,15 @@ def resolve_headers(path: str, version: str):  # NOSONAR
         "/*\n",
         "Generated from the CGMES files via cimgen: https://github.com/sogno-platform/cimgen\n",
         "*/\n",
-        "\n",
-        "using namespace CIMPP;\n",
         "#include <list>\n",
-        "static std::list<BaseClassDefiner> CIMClassList = {\n",
+        '#include "IEC61970.hpp"\n',
+        "using namespace CIMPP;\n",
+        "static std::list<BaseClassDefiner> CIMClassList =\n",
+        "{\n",
     ]
     class_list_footer = [
-        "    UnknownType::declare() };\n",
+        "	UnknownType::declare(),\n",
+        "};\n",
         "#endif // CIMCLASSLIST_H\n",
     ]
 
@@ -467,7 +480,7 @@ def resolve_headers(path: str, version: str):  # NOSONAR
         "CIMClassList.hpp",
         class_list_header,
         class_list_footer,
-        "    ",
+        "	",
         "::declare(),\n",
         class_blacklist,
     )
