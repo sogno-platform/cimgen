b9293ad7f3c36bba9559dbfcac596d0aa6cde2a8
Markus Mirz <markus.mirz@rwth-aachen.de>
2025-01-18 22:41:36 +0100

    fix pre-commit checks
    Signed-off-by: Markus Mirz <markus.mirz@rwth-aachen.de>

--- json_ld/langPack.py
+++ json_ld/langPack.py
@@ -1,6 +1,7 @@
 import os
 import chevron
 import logging
+
 logger = logging.getLogger(__name__)
 
 
@@ -14,10 +15,7 @@ def location(version):
     return "cimpy." + version + ".Base"
 
 
-base = {
-    "base_class": "Base",
-    "class_location": location
-}
+base = {"base_class": "Base", "class_location": location}
 
 template_files = [{"filename": "json_ld_template.mustache", "ext": ".json"}]
 
@@ -27,7 +25,7 @@ def get_class_location(class_name, class_map, version):
 
 
 def _set_default(text, render):
-    return '0.0'
+    return "0.0"
 
 
 def set_enum_classes(new_enum_classes):
@@ -40,18 +38,13 @@ def set_float_classes(new_float_classes):
 
 def run_template(version_path, class_details):
     for template_info in template_files:
-        class_file = os.path.join(version_path, class_details['class_name']
-                                  + template_info["ext"])
+        class_file = os.path.join(version_path, class_details["class_name"] + template_info["ext"])
         if not os.path.exists(class_file):
-            with open(class_file, 'w') as file:
-                template_path = os.path.join(os.getcwd(), 'json_ld/templates',
-                                             template_info["filename"])
-                class_details['setDefault'] = _set_default
+            with open(class_file, "w") as file:
+                template_path = os.path.join(os.getcwd(), "json_ld/templates", template_info["filename"])
+                class_details["setDefault"] = _set_default
                 with open(template_path) as f:
-                    args = {
-                        'data': class_details,
-                        'template': f
-                        }
+                    args = {"data": class_details, "template": f}
                     output = chevron.render(**args)
                 file.write(output)
 
