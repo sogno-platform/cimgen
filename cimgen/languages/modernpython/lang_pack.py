8184fb5b13fe6d57b31f7d1613343573bc9e49bb
Thomas Günther <tom@toms-cafe.de>
2025-02-03 00:31:50 +0100

    Change creation of constants for modernpython, generating now all namespaces in constants.py and correcting eu namespace for CGMES 3
    Signed-off-by: Thomas Günther <tom@toms-cafe.de>

--- cimgen/languages/modernpython/lang_pack.py
+++ cimgen/languages/modernpython/lang_pack.py
@@ -24,7 +24,7 @@ def setup(output_path: str, cgmes_profile_details: list[dict], namespaces: dict[
         dest_file = dest_dir / file.relative_to(source_dir)
         dest_file.parent.mkdir(parents=True, exist_ok=True)
         shutil.copy(file, dest_file)
-    _create_constants(dest_dir, namespaces["cim"])
+    _create_constants(dest_dir, namespaces)
     _create_cgmes_profile(dest_dir, cgmes_profile_details)
 
 
@@ -161,9 +161,10 @@ def _write_templated_file(class_file: Path, class_details: dict, template_filena
         file.write(output)
 
 
-def _create_constants(output_path: Path, cim_namespace: str) -> None:
+def _create_constants(output_path: Path, namespaces: dict[str, str]) -> None:
     class_file = output_path / "utils" / ("constants" + constants_template_file["ext"])
-    class_details = {"cim_namespace": cim_namespace}
+    namespaces_list = [{"ns": ns, "uri": uri} for ns, uri in sorted(namespaces.items())]
+    class_details = {"namespaces": namespaces_list}
     _write_templated_file(class_file, class_details, constants_template_file["filename"])
 
 
