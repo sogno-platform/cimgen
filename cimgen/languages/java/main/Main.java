3c4002f0e1b54dd6fa7526aa5a66f87bebef5931
Thomas Günther <tom@toms-cafe.de>
2025-05-25 15:15:38 +0200

    Add option --log-level to java main program
    Signed-off-by: Thomas Günther <tom@toms-cafe.de>

--- cimgen/languages/java/main/Main.java
+++ cimgen/languages/java/main/Main.java
@@ -14,34 +14,55 @@ import cim4j.utils.RdfWriter;
  */
 public final class Main {
 
-    private static final Logging LOG = Logging.getLogger(Main.class);
+    private static Logging LOG = Logging.getLogger(Main.class);
 
     // Private dummy constructor - prevent to instantiate the class at all
     private Main() {
     }
 
-    private static void printUsageAndExit(String aError) {
-        if (!aError.isEmpty()) {
-            System.out.println("\nError: " + aError);
+    private static void printUsageAndExit(String error) {
+        if (!error.isEmpty()) {
+            System.out.println("\nError: " + error);
         }
         System.out.println("\nRead RDF files and write the data to RDF files separated by profiles.\n");
-        System.out.println("Usage: java -jar cim4j.jar <rdf_file>  [<rdf_file>  ...] <output_path_stem>");
+        System.out.println("Usage: java -jar cim4j.jar [--log-level <level>] <rdf_file> [<rdf_file> ...]" +
+                " <output_path_stem>");
+        System.out.println("       --log-level <level>  Log level (fatal, error, warn, info, debug, trace)");
+        System.out.println("                            Default log level: error");
+        System.out.println("       <rdf_file> ...       Input files with CIM/CGMES data");
+        System.out.println("       <output_path_stem>   Stem of the output files" +
+                " (<output_path_stem>_<profile_name>.xml)");
         System.exit(2);
     }
 
     /**
      * Main function of the cim4j application.
      */
-    public static void main(String[] aArgs) {
-        if (aArgs.length < 2) {
+    public static void main(String[] args) {
+        int offset = 0;
+        if (args.length >= 2 && args[0].equals("--log-level")) {
+            Logging.Level level = Logging.getDefaultLogLevel();
+            try {
+                level = Logging.Level.valueOf(args[1]);
+            } catch (IllegalArgumentException ex) {
+                printUsageAndExit("unknown log level: " + args[1]);
+            }
+            if (!level.equals(Logging.getDefaultLogLevel())) {
+                Logging.setDefaultLogLevel(level);
+                LOG = Logging.getLogger(Main.class);
+            }
+            offset += 2;
+        }
+
+        if (args.length < offset + 2) {
             printUsageAndExit("too few arguments");
         }
 
         List<String> inputFiles = new ArrayList<>();
-        for (int idx = 0; idx < aArgs.length - 1; ++idx) {
-            inputFiles.add(aArgs[idx]);
+        for (int idx = offset; idx < args.length - 1; ++idx) {
+            inputFiles.add(args[idx]);
         }
-        String outputFile = aArgs[aArgs.length - 1];
+        String outputFile = args[args.length - 1];
 
         checkArgs(inputFiles);
 
@@ -70,7 +91,14 @@ public final class Main {
     public static Map<String, BaseClass> readRdf(List<String> inputFiles) {
         try {
             var rdfReader = new RdfReader();
-            return rdfReader.read(inputFiles);
+            int count = 0;
+            for (var file : inputFiles) {
+                ++count;
+                LOG.info(String.format("CIM inputfile %d: %s", count, file));
+            }
+            var cimData = rdfReader.read(inputFiles);
+            LOG.info(String.format("Read %d inputfiles", count));
+            return cimData;
         } catch (Exception ex) {
             LOG.error("Failed to convert RDF files to CIM", ex);
             return null;
@@ -94,6 +122,7 @@ public final class Main {
                 ++count;
                 LOG.info(String.format("CIM outputfile %d: %s", count, profileFileMap.get(profile)));
             }
+            LOG.info(String.format("Written %d outputfiles", count));
         } catch (Exception ex) {
             LOG.error("Failed to write CIM data to a RDF file", ex);
             return;
